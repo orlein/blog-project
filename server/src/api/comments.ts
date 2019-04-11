091a92fbb4a393292ab15e1451c7af9ccb5a8160
park changjun <pcj9024@gmail.com>
2019-04-11 22:36:13 +0900

    working on comments endpoints

--- server/src/api/comments.ts
+++ server/src/api/comments.ts
@@ -1,54 +1,110 @@
 import { Request, Response, NextFunction } from 'express';
-
+import { Util, ResponseBody, SUCCESSFUL } from '../common';
+import { Comment } from '../models/Comment';
 export abstract class CommentsController {
 
   /**
    * GET /api/v1/articles/{id}/comments?page={page}&perPage={perPage}
    */
   public static getAllCommentsFromAnArticle = async (req: Request, res: Response, next: NextFunction): Promise<Response | undefined> =>{
-    return res.status(200).send('get all comments from this article');
+    try {
+      const page = Util.safeParse(req.query.page);
+      const perPage = Util.safeParse(req.query.perPage);
+      const articleId = Util.safeParse(req.query.id);
+      const comments = await Comment.findAll({offset: page * perPage, limit: perPage, where: { articleId } });
+      const responseBody = new ResponseBody(SUCCESSFUL, comments);
+      return res.status(200).json(responseBody);
+    } catch(e) {
+      next(e);
+    }
   }
 
   /**
    * POST /api/v1/articles/{id}/comments
    */
   public static postCommentToAnArticle = async (req: Request, res: Response, next: NextFunction): Promise<Response | undefined> =>{
-    return res.status(200).send('get all comments from this article');
+    try {
+      
+      const user = await User.create<User>(req.body);
+      await user.save();
+      const responseBody = new ResponseBody(SUCCESSFUL, user);
+      return res.status(200).json(responseBody);
+    } catch (e) {
+      next(e);
+    }
   }
   
   /**
    * PATCH /api/v1/comments/{id}
    */
   public static editComment = async (req: Request, res: Response, next: NextFunction): Promise<Response | undefined> =>{
-    return res.status(200).send('edit a comment from this article');
+    try {
+      const id = Util.safeParse(req.params.id);
+      const result = await User.update(req.body, {where: { id }})
+      const responseBody = new ResponseBody(SUCCESSFUL, result);
+      return res.status(200).json(responseBody);
+    } catch(e) {
+      next(e);
+    }
   }
   
   /**
    * DELETE /api/v1/comments/{id}
    */
   public static deleteComment = async (req: Request, res: Response, next: NextFunction): Promise<Response | undefined> =>{
-    return res.status(200).send('delete a from this article');
-  }
-
-  /**
-   * POST /api/v1/comments
-   * delete many comments 
-   */
-  public static deleteManyComments = async (req: Request, res: Response, next: NextFunction): Promise<Response | undefined> =>{
-    return res.status(200).send('delete comments, but not instantly');
+    try {
+      const id = Util.safeParse(req.params.id);
+      const result = await User.update({ toBeDeleted: true }, { where: {id}});
+      const responseBody = new ResponseBody(SUCCESSFUL, result);
+      return res.status(200).json(responseBody);
+    } catch (e) {
+      next(e);
+    }
   }
 
   /**
    * POST /api/v1/comments/{id}/like
    */
   public static likeComment = async (req: Request, res: Response, next: NextFunction): Promise<Response | undefined> =>{
-    return res.status(200).send('this user likes an comment');
+    try {
+      const likerId = Util.safeParse(req.user.id);
+      const articleId = Util.safeParse(req.params.id);
+      const result = await LikeService.cancelArticleLikeDislike(likerId, articleId)(1);
+      const responseBody = new ResponseBody(SUCCESSFUL, result);
+      return res.status(200).json(responseBody);
+    } catch(e) {
+      next(e);
+    }
   }
 
   /**
    * POST /api/v1/comments/{id}/dislike
    */
   public static dislikeComment = async (req: Request, res: Response, next: NextFunction): Promise<Response | undefined> =>{
-    return res.status(200).send('this user dislikes an comment');
+    try {
+      const likerId = Util.safeParse(req.user.id);
+      const articleId = Util.safeParse(req.params.id);
+      const result = await LikeService.cancelArticleLikeDislike(likerId, articleId)(-1);
+      const responseBody = new ResponseBody(SUCCESSFUL, result);
+      return res.status(200).json(responseBody);
+    } catch(e) {
+      next(e);
+    }
   }
+
+
+  /**
+   * POST /api/v1/comments/{id}/cancel
+   */
+  public static cancelLikeComent = async (req: Request, res: Response, next: NextFunction): Promise<Response | undefined> =>{
+    try {
+      const likerId = Util.safeParse(req.user.id);
+      const articleId = Util.safeParse(req.params.id);
+      const result = await LikeService.cancelArticleLikeDislike(likerId, articleId)(0);
+      const responseBody = new ResponseBody(SUCCESSFUL, result);
+      return res.status(200).json(responseBody);
+    } catch(e) {
+      next(e);
+    }
+  } 
 }
