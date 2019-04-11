6eca985d1fd165be1081940f7d1c837bbf879b1f
park changjun <pcj9024@gmail.com>
2019-04-11 22:36:05 +0900

    update likeservice

--- server/src/services/LikeService.ts
+++ server/src/services/LikeService.ts
@@ -1,6 +1,7 @@
 import { UsersLikeArticles } from '../models/UsersLikeArticles';
+import { UsersLikeComments } from '../models/UsersLikeComments';
 export abstract class LikeService {
-  public static likeDislikeCancel = (userId: number, articleId: number) => async (ldc: number): Promise<any> => {
+  public static cancelArticleLikeDislike = (userId: number, articleId: number) => async (ldc: number): Promise<any> => {
     const userLikeArticle = await UsersLikeArticles.findOne({ where: { userId: userId, articleId: articleId }})
     if(userLikeArticle) {
       const result = await UsersLikeArticles.update(
@@ -19,4 +20,23 @@ export abstract class LikeService {
       return result;
     }
   }
+
+  public static cancelCommentLikeDislike = (userId: number, commentId: number) => async (ldc: number): Promise<any> => {
+    const userLikeComment = await UsersLikeComments.findOne({ where: { userId: userId, commentId: commentId }})
+    if(userLikeComment) {
+      const result = await UsersLikeArticles.update(
+        { likeOrDislike: -1 }, 
+        { where: { 
+          userId: userId, 
+          commentId: commentId }
+        })
+      return result;
+    } else {
+      const result = await UsersLikeArticles.create<UsersLikeArticles>({
+        userId: userId,
+        commentId: commentId,
+        likeOrDislike: -1
+      })
+      return result;
+    }
 }
