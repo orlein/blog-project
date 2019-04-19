e3c7354d964dd385d97076acf3dc8a51f27a234f
orlein <pcj9024@gmail.com>
2019-04-19 15:40:15 +0900

    update jwtSecret

--- server/src/config/keys.ts
+++ server/src/config/keys.ts
@@ -1,1 +1,2 @@
-export const cookieSecret: string = String(process.env.cookieSecret) || '5bBWOWg6kmiQNeNAVUNw';
+export const cookieSecret: string = String(process.env.COOKIE_SECRET) || '5bBWOWg6kmiQNeNAVUNw';
+export const jwtSecret: string = String(process.env.JWT_SECRET) || '8jbGcMfwK4aX2Vjq/yw/MQ==';
