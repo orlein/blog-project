1e6e06ef2f4076130d5c62b247f06f41191b0b22
orlein <pcj9024@gmail.com>
2019-04-19 18:44:58 +0900

    working on connecting api to router object

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -2,7 +2,9 @@ import { Router, Request, Response, NextFunction } from 'express';
 import { UsersController } from './users';
 const router = Router();
 
+router.post('/v1/login', UsersController.login);
 router.get('/v1/users', UsersController.getUsers);
 router.post('/v1/users', UsersController.registerSingleUser);
+
 
 export default router;
